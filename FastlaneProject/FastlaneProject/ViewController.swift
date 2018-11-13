3de50c8f4629e40804d91485cbfbeaab6be60cd7
İkbal Yaşar <miyasar@gtu.edu.tr>
2018-11-13 13:56:29 +0300

    First Commit

---
+++ FastlaneProject/FastlaneProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FastlaneProject
+//
+//  Created by İkbal Yaşar on 11/13/18.
+//  Copyright © 2018 İkbal Yaşar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
